9f9670b11800c3da8ab37eaa5ad9e8b9879be9af
cathjime <xcatherine.jimenez@gmail.com>
2020-06-16 13:06:32 -0400

    success page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get '/', to: 'application#welcome', as: "home"
   get '/transition', to: 'flow#transition', as: "flow"
+  get '/success', to: 'flow#success', as: "success"
+  
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
