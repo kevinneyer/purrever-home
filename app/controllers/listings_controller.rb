4bb0d56e758b4446396533f17d22ca96501b0fba
cathjime <xcatherine.jimenez@gmail.com>
2020-06-15 20:53:20 -0400

    views

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,45 @@
 class ListingsController < ApplicationController
+before_action :find_listing, only: [:show, :edit, :update, :destroy]
+
     def index
         @listings = Listing.all 
     end
 
     def show
-        @listing = Listing.find(params[:id])
+       
+        @cat = @listing.cat
     end
 
+    def new
+        @listing = Listing.new
+        @cats = Cat.all
+        @adoption_centers = AdoptionCenter.all
+    end
+
+    def create
+       
+        @listing = Listing.create(listings_params)
+        redirect_to listing_path(@listing)
+    end
+
+    def edit
+        @cats = Cat.all
+        @adoption_centers = AdoptionCenter.all
+    end
+
+    def update
+        @listing.update(listings_params)
+        redirect_to listing_path(@listing)
+    end
+private
+
+def find_listing
+    @listing = Listing.find(params[:id])
 end
+
+def listings_params
+    params.require(:listing).permit!
+end 
+
+
+end 
