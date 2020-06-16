928a5e0d6555a7bbb60d01158c69eee4016ec75d
kevinneyer <kevinneyer@gmail.com>
2020-06-16 17:19:25 -0400

    password digest added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_214034) do
+ActiveRecord::Schema.define(version: 2020_06_16_211443) do
 
   create_table "adoption_centers", force: :cascade do |t|
     t.string "name"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_214034) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
