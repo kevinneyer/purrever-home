4e5567375564a3ce74d0457b565a2c8a619eadcb
kevinneyer <kevinneyer@gmail.com>
2020-06-16 22:17:50 -0400

    tried sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   def welcome
   end
 
-  # def get_user
-  #   current_user = User.find_by(id: session[:username_id])
-  # end
+  def find_user
+   current_user =  User.find_by(id: session[:username_id])
+  end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
 
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
+      session[:username_id] = user
 
       redirect_to listings_path
     else
