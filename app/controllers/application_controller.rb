e2836728516b55aea52905ce0bcc58f8a157ef48
kevinneyer <kevinneyer@gmail.com>
2020-06-16 22:46:16 -0400

    final night tryings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   def welcome
   end
 
-  def find_user
-   current_user =  User.find_by(id: session[:username_id])
-  end
+
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
 
     if user && user.authenticate(params[:session][:password])
-      session[:username_id] = user
+      session[:user_id] = user.id
 
       redirect_to listings_path
     else
