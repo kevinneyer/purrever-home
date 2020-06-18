74eedcc0b93af557829b871664e08bab28ecc45c
kevinneyer <kevinneyer@gmail.com>
2020-06-18 11:17:52 -0400

    Thursday Morning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
 
+  before_action :logged_in?
+  
   def welcome
   end
 
-
+  def current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+  
+  def logged_in?
+    !current_user.nil?
+  end
 
 end
